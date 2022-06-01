14a879d1cf643e9420bee457836a884bd2f46af3
Shon <sparkworrior@gmail.com>
2022-06-01 18:38:28 -0400

    Comdet Code

---
+++ Code that works/python-louvain-master/write_edgelist.py
@@ -0,0 +1,24 @@
+from community import community_louvain
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+import networkx as nx
+import time
+
+start = time.time()
+# load the karate club graph
+G = nx.write_edgelist('1e5_5e5.edgelist', nodetype=int, data=(('weight', float)))
+
+# compute the best partition
+partition = community_louvain.best_partition(G)
+
+# draw the graph
+pos = nx.spring_layout(G)
+# color the nodes according to their partition
+cmap = cm.get_cmap('viridis', max(partition.values()) + 1)
+nx.draw_networkx_nodes(G, pos, partition.keys(), node_size=40,
+                       cmap=cmap, node_color=list(partition.values()))
+nx.draw_networkx_edges(G, pos, alpha=0.5)
+end = time.time()
+plt.show()
+total_time = end - start
+print(total_time)
